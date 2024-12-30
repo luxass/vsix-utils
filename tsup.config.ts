e74b7ce1cda782dc46c890ae0903ffe182859e8c
Lucas <lucasnrgaard@gmail.com>
2024-12-30 08:49:49 +0100

    chore: lint

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
-  format: ['cjs', 'esm'],
+  entry: ["./src/index.ts"],
+  format: ["cjs", "esm"],
   clean: true,
   dts: true,
   treeshake: true,
   bundle: true,
   outExtension(ctx) {
     return {
-      js: ctx.format === 'cjs' ? '.cjs' : '.mjs',
-    }
+      js: ctx.format === "cjs" ? ".cjs" : ".mjs",
+    };
   },
-})
+});
