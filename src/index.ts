f459e7a9ca02880e5cb7e1b4abd316a48aa403da
Lucas <lucasnrgaard@gmail.com>
2024-12-31 04:49:56 +0100

    fix: add validation to index exports

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,26 @@ export {
   isLocalFile,
 } from "./files";
 
+export type { Manifest } from "./types";
+
+export type { ManifestValidation } from "./validation";
+export {
+  ALLOWED_SPONSOR_PROTOCOLS,
+  EXTENSION_NAME_REGEX,
+  EXTENSION_PRICING,
+  GITHUB_BADGE_URL_REGEX,
+  VALID_EXTENSION_KINDS,
+  validateProjectManifest,
+  validateVSCodeTypesCompatability,
+  VSCODE_ENGINE_COMPATIBILITY_REGEX,
+} from "./validation";
+
 export type {
   RawVsixPackage,
   ReadVsixOptions,
   WriteVsixOptions,
 } from "./zip";
+
 export {
   readVsix,
   writeVsix,
