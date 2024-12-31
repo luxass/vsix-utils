92987c84debab88a7ac6755b0d43c681859f3bfa
Lucas <lucasnrgaard@gmail.com>
2024-12-31 05:45:56 +0100

    chore: add get extension dependencies method

--- src/files.ts
+++ src/files.ts
@@ -4,6 +4,8 @@
  */
 
 import type { Buffer } from "node:buffer";
+import type { Manifest, PackageManager } from "./types";
+import process from "node:process";
 
 export interface VsixLocalFile {
   type: "local";
@@ -25,4 +27,43 @@ export function isLocalFile(file: VsixFile): file is VsixLocalFile {
 
 export function isInMemoryFile(file: VsixFile): file is VsixInMemoryFile {
   return file.type === "in-memory";
+}
+
+export interface ExtensionDependenciesOptions {
+  /**
+   * The package manager to use.
+   */
+  packageManager: PackageManager;
+
+  /**
+   * The current working directory
+   * @default process.cwd()
+   */
+  cwd: string;
+}
+
+export interface ExtensionDependenciesResult {
+  /**
+   * The dependencies of the extension.
+   */
+  dependencies: string[];
+
+  /**
+   * The package manager used to resolve the dependencies.
+   */
+  packageManager: PackageManager;
+}
+
+export async function getExtensionDependencies(manifest: Manifest, options: ExtensionDependenciesOptions): Promise<ExtensionDependenciesResult | null> {
+  const {
+    packageManager: pm,
+    cwd = process.cwd(),
+  } = options;
+
+  const packageManager = pm;
+
+  return {
+    dependencies: [],
+    packageManager,
+  };
 }
