b0fcf7e980d99dfcd1ea375b6fb9d9323ff770c7
Lucas <lucasnrgaard@gmail.com>
2024-12-31 13:29:48 +0100

    feat: handle pnpm & yarn in package manager detection

--- src/files.ts
+++ src/files.ts
@@ -6,7 +6,7 @@
 import type { Buffer } from "node:buffer";
 import type { Manifest, PackageManager } from "./types";
 import { exec } from "node:child_process";
-import path, { isAbsolute, resolve } from "node:path";
+import path, { isAbsolute, join, resolve } from "node:path";
 import process from "node:process";
 import { promisify } from "node:util";
 
@@ -77,6 +77,26 @@ export interface ExtensionDependenciesResult {
   packageManager: Exclude<PackageManager, "auto"> | null;
 }
 
+interface PnpmDependency {
+  from: string;
+  version: string;
+  resolved: string;
+  path: string;
+  dependencies: Record<string, PnpmDependency>;
+}
+
+interface YarnTreeNode {
+  name: string;
+  children: YarnTreeNode[];
+}
+
+export interface YarnDependency {
+  name: string;
+  path: string;
+  version: string;
+  children: YarnDependency[];
+}
+
 export async function getExtensionDependencies(manifest: Manifest, options: ExtensionDependenciesOptions): Promise<ExtensionDependenciesResult> {
   const {
     packageManager: pm = "auto",
@@ -129,9 +149,141 @@ export async function getExtensionDependencies(manifest: Manifest, options: Exte
       });
     }
   } else if (packageManager === "yarn") {
-    throw new Error("yarn is not supported yet");
+    const { stdout } = await execAsync("yarn list --prod --json", { cwd });
+
+    const match = /^\{"type":"tree".*$/m.exec(stdout);
+
+    if (!match || match.length !== 1) {
+      throw new Error("Could not parse result of `yarn list --json`");
+    }
+
+    const trees = JSON.parse(match[0]).data.trees as YarnTreeNode[];
+
+    if (!Array.isArray(trees) || trees.length === 0) {
+      return {
+        dependencies: [],
+        packageManager,
+      };
+    }
+
+    const prune = true; // TODO: using packaged dependencies
+
+    function asYarnDependency(prefix: string, tree: YarnTreeNode, prune: boolean): YarnDependency | null {
+      if (prune && /@[\^~]/.test(tree.name)) {
+        return null;
+      }
+
+      let name: string;
+      let version: string = "";
+      try {
+        const tmp = tree.name.split("@");
+
+        if (tmp[0] === "") {
+          tmp.shift();
+          tmp[0] = `@${tmp[0]}`;
+        }
+
+        name = tmp[0]!;
+        version = tmp[1] || "";
+      } catch {
+        name = tree.name.replace(/^([^@+])@.*$/, "$1");
+      }
+
+      const dependencyPath = path.join(prefix, name);
+      const children: YarnDependency[] = [];
+
+      for (const child of tree.children || []) {
+        const dep = asYarnDependency(path.join(prefix, name, "node_modules"), child, prune);
+
+        if (dep) {
+          children.push(dep);
+        }
+      }
+
+      return { name, path: dependencyPath, children, version };
+    }
+
+    const result = trees.map((tree) => asYarnDependency(join(cwd, "node_modules"), tree, prune)).filter((dep) => dep != null);
+
+    const internalDeps = new Set<string>();
+
+    const flatten = (dep: YarnDependency) => {
+      if (internalDeps.has(dep.path)) {
+        return;
+      }
+
+      dependencies.add({
+        name: dep.name,
+        version: dep.version,
+        path: dep.path,
+      });
+      internalDeps.add(dep.path);
+
+      if (dep.children) {
+        for (const child of dep.children) {
+          flatten(child);
+        }
+      }
+    };
+
+    for (const dep of result) {
+      flatten(dep);
+    }
   } else if (packageManager === "pnpm") {
-    throw new Error("pnpm is not supported yet");
+    const { stdout } = await execAsync("pnpm list --production --json --depth=99999 --loglevel=error", { cwd });
+
+    let entryList = [];
+    try {
+      entryList = JSON.parse(stdout);
+    } catch {
+      return {
+        dependencies: [],
+        packageManager,
+      };
+    }
+
+    if (!Array.isArray(entryList) || entryList.length === 0) {
+      return {
+        dependencies: [],
+        packageManager,
+      };
+    }
+
+    const entry = entryList[0] as {
+      dependencies?: Record<string, PnpmDependency>;
+    };
+
+    if (entry == null || typeof entry !== "object" || entry.dependencies == null || typeof entry.dependencies !== "object") {
+      return {
+        dependencies: [],
+        packageManager,
+      };
+    }
+
+    const internalDeps = new Set<string>();
+
+    const flatten = (dep: PnpmDependency) => {
+      if (internalDeps.has(dep.path)) {
+        return;
+      }
+
+      dependencies.add({
+        name: dep.from,
+        version: dep.version,
+        path: dep.path,
+      });
+      internalDeps.add(dep.path);
+
+      if (dep.dependencies) {
+        for (const child of Object.values(dep.dependencies)) {
+          flatten(child);
+        }
+      }
+    };
+
+    for (const value of Object.values(entry.dependencies)) {
+      flatten(value);
+    }
   } else {
     throw new Error(`unsupported package manager: ${packageManager}`);
   }
--- test/package-manager.test.ts
+++ test/package-manager.test.ts
@@ -8,8 +8,35 @@ import { readProjectManifest } from "../src/manifest";
 
 const execAsync = promisify(exec);
 
+async function hasNpm() {
+  try {
+    await execAsync("npm --version");
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function hasYarn() {
+  try {
+    await execAsync("yarn --version");
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function hasPnpm() {
+  try {
+    await execAsync("pnpm --version");
+    return true;
+  } catch {
+    return false;
+  }
+}
+
 // timeout should be 20s as npm can take a bit to install
-describe("npm", { timeout: 20000 }, () => {
+describe.runIf(await hasNpm())("npm", { timeout: 20000 }, () => {
   it("should throw an error if unsupported package manager is provided", async () => {
     const fsFiles = await fromFileSystem("./test/fixtures/package-manager/npm/no-dependencies");
     const dir = await testdir(fsFiles);
@@ -94,6 +121,186 @@ describe("npm", { timeout: 20000 }, () => {
     });
 
     expect(packageManager).toEqual("npm");
+    expect(dependenciesWithRelative).toMatchSnapshot();
+  });
+});
+
+describe.runIf(await hasYarn())("yarn", { timeout: 20000 }, () => {
+  it("should throw an error if unsupported package manager is provided", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/yarn/no-dependencies");
+    const dir = await testdir(fsFiles);
+
+    const { manifest } = await readProjectManifest(dir);
+
+    await expect(getExtensionDependencies(manifest, {
+      cwd: dir,
+      // @ts-expect-error just testing for unsupported package manager
+      packageManager: "custom",
+    })).rejects.toThrow("unsupported package manager: custom");
+  });
+
+  // TODO: currently the package manager detect is traversing up the directory
+  //       and therefor will locate the package.json in the root of the project
+  //       instead of stopping at the test directory. So this test will fail until
+  //       we have a otion to stop the traversing.
+  //       PR: https://github.com/antfu-collective/package-manager-detector/pull/39
+  it.todo("should throw an error if package is auto and can't be detected", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/yarn/no-dependencies");
+    const dir = await testdir(fsFiles);
+
+    const { manifest } = await readProjectManifest(dir);
+
+    await expect(getExtensionDependencies(manifest, {
+      cwd: dir,
+      packageManager: "auto",
+    })).rejects.toThrow("unable to detect package manager");
+  });
+
+  it("should default to auto if package manager is not provided", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/yarn/no-dependencies");
+    const dir = await testdir(fsFiles, {
+      cleanup: false,
+    });
+
+    await execAsync("yarn install", { cwd: dir });
+
+    const { manifest } = await readProjectManifest(dir);
+
+    const { dependencies, packageManager } = await getExtensionDependencies(manifest, {
+      cwd: dir,
+    });
+
+    expect(dependencies).toEqual([]);
+
+    expect(packageManager).toEqual("yarn");
+  });
+
+  it("should handle no dependencies correctly", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/yarn/no-dependencies");
+    const dir = await testdir(fsFiles);
+
+    await execAsync("yarn install", { cwd: dir });
+
+    const { manifest } = await readProjectManifest(dir);
+
+    const { dependencies, packageManager } = await getExtensionDependencies(manifest, {
+      cwd: dir,
+      packageManager: "yarn",
+    });
+
+    expect(dependencies).toEqual([]);
+
+    expect(packageManager).toEqual("yarn");
+  });
+
+  it("should handle dependencies correctly", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/yarn/with-dependencies");
+    const dir = await testdir(fsFiles);
+
+    await execAsync("yarn install", { cwd: dir });
+
+    const { manifest } = await readProjectManifest(dir);
+
+    const { dependencies, packageManager } = await getExtensionDependencies(manifest, {
+      cwd: dir,
+      packageManager: "yarn",
+    });
+
+    // prevent issues with running tests locally, as the path will be different
+    const dependenciesWithRelative = dependencies.map((dep) => {
+      return dep.path.replace(resolve(`${dir}/../../`), "").slice(1).replace(/\\/g, "/");
+    });
+
+    expect(packageManager).toEqual("yarn");
+    expect(dependenciesWithRelative).toMatchSnapshot();
+  });
+});
+
+describe.runIf(await hasPnpm())("pnpm", { timeout: 20000 }, () => {
+  it("should throw an error if unsupported package manager is provided", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/pnpm/no-dependencies");
+    const dir = await testdir(fsFiles);
+
+    const { manifest } = await readProjectManifest(dir);
+
+    await expect(getExtensionDependencies(manifest, {
+      cwd: dir,
+      // @ts-expect-error just testing for unsupported package manager
+      packageManager: "custom",
+    })).rejects.toThrow("unsupported package manager: custom");
+  });
+
+  // TODO: currently the package manager detect is traversing up the directory
+  //       and therefor will locate the package.json in the root of the project
+  //       instead of stopping at the test directory. So this test will fail until
+  //       we have a otion to stop the traversing.
+  //       PR: https://github.com/antfu-collective/package-manager-detector/pull/39
+  it.todo("should throw an error if package is auto and can't be detected", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/pnpm/no-dependencies");
+    const dir = await testdir(fsFiles);
+
+    const { manifest } = await readProjectManifest(dir);
+
+    await expect(getExtensionDependencies(manifest, {
+      cwd: dir,
+      packageManager: "auto",
+    })).rejects.toThrow("unable to detect package manager");
+  });
+
+  it("should default to auto if package manager is not provided", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/pnpm/no-dependencies");
+    const dir = await testdir(fsFiles);
+
+    await execAsync("pnpm install", { cwd: dir });
+
+    const { manifest } = await readProjectManifest(dir);
+
+    const { dependencies, packageManager } = await getExtensionDependencies(manifest, {
+      cwd: dir,
+    });
+
+    expect(dependencies).toEqual([]);
+
+    expect(packageManager).toEqual("pnpm");
+  });
+
+  it("should handle no dependencies correctly", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/pnpm/no-dependencies");
+    const dir = await testdir(fsFiles);
+
+    await execAsync("pnpm install", { cwd: dir });
+
+    const { manifest } = await readProjectManifest(dir);
+
+    const { dependencies, packageManager } = await getExtensionDependencies(manifest, {
+      cwd: dir,
+      packageManager: "pnpm",
+    });
+
+    expect(dependencies).toEqual([]);
+
+    expect(packageManager).toEqual("pnpm");
+  });
+
+  it("should handle dependencies correctly", async () => {
+    const fsFiles = await fromFileSystem("./test/fixtures/package-manager/pnpm/with-dependencies");
+    const dir = await testdir(fsFiles);
+
+    await execAsync("pnpm install", { cwd: dir });
+
+    const { manifest } = await readProjectManifest(dir);
+
+    const { dependencies, packageManager } = await getExtensionDependencies(manifest, {
+      cwd: dir,
+      packageManager: "pnpm",
+    });
+
+    // prevent issues with running tests locally, as the path will be different
+    const dependenciesWithRelative = dependencies.map((dep) => {
+      return dep.path.replace(resolve(`${dir}/../../`), "").slice(1).replace(/\\/g, "/");
+    });
+
+    expect(packageManager).toEqual("pnpm");
     expect(dependenciesWithRelative).toMatchSnapshot();
   });
 });
